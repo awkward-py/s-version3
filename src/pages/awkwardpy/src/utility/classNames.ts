1fd678038316a43ffe625c3981ce4f0adae06c9c
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-09-22 22:11:07 +0530

    Delete src/pages/awkwardpy directory

--- src/pages/awkwardpy/src/utility/classNames.ts
+++
@@ -1,5 +0,0 @@
-export function classNames(
-  ...classes: Array<string | boolean | false | undefined | null>
-): string {
-  return classes.filter(Boolean).join(" ");
-}
--- src/pages/awkwardpy/src/utility/verifyEmail.ts
+++
@@ -1,5 +0,0 @@
-export function verifyEmailAddress(email: string) {
-  // This regex is not up to the standard of Google's RFC200 email regex this is a soft verification
-  let regex = /^[^@]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{1,}$/;
-  return regex.test(email);
-}
